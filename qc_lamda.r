d7052536459720e764702d5397b29044ad3cd396
wuame <69006363+wuame@users.noreply.github.com>
2024-06-02 19:08:41 -0400

    Delete qc_lamda.r

--- qc_lamda.r
+++
@@ -1,7 +0,0 @@
-data<-read.table("/path", h=T, as.is=T)
-names(data)[names(data) == 'P.value'] <- 'P'
-p=data$P # p means p-value column name
-Zsq=qchisq(1-p, 1)
-lambda=median(Zsq)/0.456
-lambda 
-
