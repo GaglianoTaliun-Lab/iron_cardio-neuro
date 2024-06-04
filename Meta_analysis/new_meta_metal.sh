47bf1fd83abc8e5dd2ecbb6a54bcf76cd382b04d
wuame <69006363+wuame@users.noreply.github.com>
2024-06-03 21:36:17 -0400

    Update meta_analysis_metal.sh

--- Meta_analysis/new_meta_metal.sh
+++ Meta_analysis/new_meta_metal.sh
@@ -1,41 +1,42 @@
 
-#  THE SECOND BENYAMIN FILE
+
+#  THE FIRST BENYAMIN FILE
 MARKER chpos
 ALLELE A1 A2
 EFFECT EFFECT_A1
 PVALUE P
 WEIGHT N
-PROCESS /home/wuaame/projects/def-gsarah/wuaame/final_metal/final/benyamin_update.txt
+PROCESS /home/wuaame/projects/def-gsarah/wuaame/final_metal/final/iron_benyamin.txt
 
 
-#THE FOURTH BLSA FILE
+#THE SECOND BLSA FILE
 MARKER SNP
 ALLELE A1 A2
 EFFECT BETA
 PVALUE P
 WEIGHT NMISS
-PROCESS /home/wuaame/projects/def-gsarah/wuaame/final_metal/final/blsa_update.txt
+PROCESS /home/wuaame/projects/def-gsarah/wuaame/final_metal/final/iron_BLSA.txt
 
 
-#THE ICM INPUT FILE
+#THE THIRD ICM FILE
 MARKER chpos
 ALLELE Allele2 Allele1
 EFFECT BETA
 PVALUE Pvalue
 WEIGHT N
-PROCESS /home/wuaame/projects/def-gsarah/wuaame/icm_converted5.txt
+PROCESS /home/wuaame/projects/def-gsarah/wuaame/iron_icm.txt
 
-#THE MOKSNES INPUT FILE
+#THE FOURTH MOKSNES FILE
 
 MARKER chpos
 ALLELE effect_allele other_allele
 EFFECT beta
 PVALUE p_value
 WEIGHT N
-PROCESS /home/wuaame/projects/def-gsarah/wuaame/moksnes/iron_moksens37_cnv3.txt
+PROCESS /home/wuaame/projects/def-gsarah/wuaame/moksnes/iron_moksens37.txt
 
 
-OUTFILE NEWMETANALYSIS.tbl
+OUTFILE METANALYSIS.tbl
 AVERAGEFREQ ON
 MINMAXFREQ ON
 ANALYZE
